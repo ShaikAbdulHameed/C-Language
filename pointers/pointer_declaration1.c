4d9ae53b6622937b74803cf12f69a7397e2303a9
ShaikAbdulHameed <69207070+ShaikAbdulHameed@users.noreply.github.com>
2023-06-11 13:50:30 +0530

    Delete pointers directory

--- pointers/pointer_declaration1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int age=32;
-    int* ptr=&age;  //regular use like this int* ptr= &age equals to scanf("%d",&age); it means it takes the value.
-    //int *ptr=&age;    // int* ptr=&age and int *ptr=&age are same but we get little bit confusion
-    printf("%d",*ptr); //it prints the value
-
-}
--- pointers/pointer_deep_explanation.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    //---pointer is works like scanf -----
-
-    *p scans the address and value of a variable. here '*p' prints 'value' and 'p' prints 'address'.
-}
